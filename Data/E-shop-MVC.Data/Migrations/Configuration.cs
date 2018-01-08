795572324c3af55062a67f69566e4fb883928ff1
Luba Angelova <liuba_angelova@outlook.com>
2018-01-08 12:25:16 +0200

    Seed method

--- Data/E-shop-MVC.Data/Migrations/Configuration.cs
+++ Data/E-shop-MVC.Data/Migrations/Configuration.cs
@@ -10,12 +10,25 @@ namespace E_shop_MVC.Data.Migrations
     {
         public Configuration()
         {
-            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationsEnabled = true;
             this.AutomaticMigrationDataLossAllowed = false;
         }
 
         protected override void Seed(ApplicationDbContext context)
         {
+            context.ProductsCategories.AddOrUpdate(x => x.Id,
+               new ProductCategory() { Id = 1, Name = "Furnitures" },
+               new ProductCategory() { Id = 2, Name = "Cars" },
+               new ProductCategory() { Id = 3, Name = "Technologies" }
+               );
+
+            context.Products.AddOrUpdate(x => x.Id,
+                new Product() { Id = 1, CategoryId = 1, Title = "Table", Content = "Kitchen table", Price = 50 },
+                new Product() { Id = 2, CategoryId = 1, Title = "Chair", Content = "Set ot 4 chairs", Price = 100 },
+                new Product() { Id = 3, CategoryId = 2, Title = "BMW", Content = "X5", Price = 50000 },
+                new Product() { Id = 4, CategoryId = 2, Title = "Opel", Content = "Meriva", Price = 5000 },
+                new Product() { Id = 5, CategoryId = 3, Title = "TV", Content = "Samsung", Price = 1200 }
+                );
         }
     }
 }
